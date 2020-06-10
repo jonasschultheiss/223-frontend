fa649053b4231f6dc1745784a4a433cccb7ef607
jonasschultheiss <jonas.raphael.schultheiss@gmail.com>
2020-06-10 13:41:47 +0200

    :sparkles: init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar></Navbar>
+      <Switch>
+        <Route exact path="/"></Route>
+        <Route exact path="/signin"></Route>
+        <Route exact path="/signup"></Route>
+        <Route exact path="/user/:id"></Route>
+        <Route exact path="/dashboard"></Route>
+        <Route exact path="/post/:id"></Route>
+        <Route exact path="/post/create"></Route>
+        <Route exact path="/post/:id/delete"></Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function () {
+  return (
+    <>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
+      <hr />
+    </>
+  );
+}
