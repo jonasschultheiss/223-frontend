3f71e6fc90f06b13ef80555caad3c42494528b0c
jonasschultheiss <jonas.raphael.schultheiss@gmail.com>
2020-06-13 08:18:33 +0200

    :sparkles: added Typography component

---
+++ src/components/Typography/Typography.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+export default function (props) {
+  const { size, shouldBeBold, shouldBeItalic, shouldBeCentered, shouldBeLight, isLink } = props;
+
+  let styles = 'font-sans';
+
+  if (size === 's') {
+    styles += ' text-sm';
+  } else if (size === 'm') {
+    styles += ' text-base';
+  } else if (size === 'l') {
+    styles += ' text-lg';
+  } else if (size === 'xl') {
+    styles += ' text-4xl';
+  }
+
+  if (shouldBeBold) {
+    styles += ' font-bold';
+  } else {
+    styles += ' font-medium';
+  }
+  if (shouldBeItalic) {
+    styles += ' italic';
+  } else {
+    styles += ' not-italic';
+  }
+  if (shouldBeCentered) {
+    styles += ' text-center';
+  }
+
+  if (shouldBeLight) {
+    styles += ' text-gray-300';
+  } else {
+    styles += ' text-blue-700';
+  }
+
+  if (isLink && shouldBeLight) {
+    styles += ' hover:text-gray-400';
+  } else if (isLink) {
+    styles += ' hover:text-blue-900';
+  }
+
+  return <span className={styles}>{props.children}</span>;
+}
