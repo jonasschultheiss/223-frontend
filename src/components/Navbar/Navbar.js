f48d0b3ca5de002b52b8aba6f4f40654e85cde8b
jonasschultheiss <jonas.raphael.schultheiss@gmail.com>
2020-06-13 08:18:56 +0200

    :sparkles: added navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Typography from '../Typography';
 
-export default function () {
+import { ReactComponent as Logo } from '../../assets/pants.svg';
+
+export default function (props) {
+  const { isAdmin = true } = props;
+
+  let dashboardLink;
+
+  isAdmin
+    ? (dashboardLink = (
+        <Link to="/dashboard" className="flex items-center justify-start">
+          <Typography size="l" shouldBeBold isLink>
+            Visit Dashboard
+          </Typography>
+        </Link>
+      ))
+    : (dashboardLink = null);
+
   return (
-    <>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/dashboard">Dashboard</Link>
-        </li>
-      </ul>
-      <hr />
-    </>
+    <header className="border-b md:flex md:items-center md:justify-between shadow-lg px-10  pt-6 pb-4">
+      <Link to="/" className="flex items-center justify-start">
+        <Logo className="w-8 h-8 mr-2 text-blue-700 fill-current" />
+        <Typography size="xl" shouldBeBold isLink>
+          Frozen Pants
+        </Typography>
+      </Link>
+      {dashboardLink}
+      <div className="flex items-center justify-end">
+        <Link to="/signup" className="rounded-md border-2 py-1 px-2 mr-4 border-blue-700 hover:border-blue-900">
+          <Typography size="l" shouldBeBold isLink>
+            Sign Up
+          </Typography>
+        </Link>
+        <Link to="/signin" className="rounded-md border-2 py-1 px-2 border-blue-700 hover:border-blue-900">
+          <Typography size="l" shouldBeBold isLink>
+            Sign In
+          </Typography>
+        </Link>
+      </div>
+    </header>
   );
 }
